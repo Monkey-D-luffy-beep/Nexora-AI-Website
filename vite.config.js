549584be2888a9358222bc8756373901445cb5d7
Saurav Payal <saurav.chaudhary70@gmail.com>
2025-09-10 18:23:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/Nexora-AI-Website/", // 👈 important for GitHub Pages
+  base: "/Nexora-AI-Website/", // 👈 this matches your repo name
 });
